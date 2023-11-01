4a62303cb8c2fb2b2835d60efb9b4f8c2c2698ee
Cheng Chain Lin <D1012337@o365.fcu.edu.tw>
2023-11-01 15:00:18 +0800

    update build.gradle.kts
    add `application` plugin
    change encoding to UTF-8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "com.github.Ttggg5"
@@ -12,6 +13,18 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
 }
 
 tasks.test {
